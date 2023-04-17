29a85d7205868082836615c627d235468e5057a7
sdy623 <syc@nbsyc.com>
2023-04-17 10:07:59 +0800

    bug fix: 修复了一些bug
    1. 修复了程序逻辑现存的bug
    2. 已删除注释中个人Windows用户名信息
    3. 订正了README.md中的拼写错误
    4. 增加了 requirements.txt 来锁定依赖版本，以此解决因PySide6 6.5.0版本更新
    导致的问题（程序报错无法运行）。

--- mainWindow.py
+++ mainWindow.py
@@ -9,17 +9,19 @@
 ################################################################################
 
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QCheckBox, QCommandLinkButton, QHBoxLayout,
-    QLabel, QLineEdit, QMainWindow, QMenuBar,
-    QPlainTextEdit, QSizePolicy, QStatusBar, QToolButton,
-    QWidget)
+                            QMetaObject, QObject, QPoint, QRect, QSize, Qt,
+                            QTime, QUrl)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+                           QFontDatabase, QGradient, QIcon, QImage,
+                           QKeySequence, QLinearGradient, QPainter, QPalette,
+                           QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QCommandLinkButton,
+                               QHBoxLayout, QLabel, QLineEdit, QMainWindow,
+                               QMenuBar, QPlainTextEdit, QSizePolicy,
+                               QStatusBar, QToolButton, QWidget)
+
 import resources_rc
+
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
